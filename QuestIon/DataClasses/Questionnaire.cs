acdaa2ebe7e1a3c8fd12ed9438f0643adcb312f3
RahlForge <rahlforge@yahoo.com>
2017-10-24 17:42:49 -0400

    Initial checkin
    Setting up project for new Quest Software scripting/survey app to
    replace FNOL

---
+++ QuestIon/DataClasses/Questionnaire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestIon.DataClasses
+{
+    public class Questionnaire
+    {        
+        protected List<QuestionnaireEntry> questionnaire;
+
+        public int Id { get; protected set; }
+
+        public Questionnaire()
+        {
+
+        }
+    }
+}
---
+++ QuestIon/DataClasses/QuestionnaireKeyValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestIon.DataClasses
+{
+    public class QuestionnaireEntry
+    {
+        protected Type dataType;
+        protected int sortOrder;        
+
+        public int Id { get; protected set; }
+        public string Text { get; protected set; }
+        public string Response { get; set; }
+        public bool Required { get; protected set; }
+
+        public QuestionnaireEntry()
+        {
+
+        }
+    }
+}
